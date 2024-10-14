4094a8b8aaeb91304c3d91ac9547258968652380
Ndiforamang Fusi <fusisoft@gmail.com>
2024-10-14 12:10:37 -0500

    added images for consulting websites

--- src/main/java/com/mt/services/ContactController.java
+++ src/main/java/com/mt/services/ContactController.java
@@ -1,9 +1,10 @@
-package com.mt.controllers;
-
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Controller
 public class ContactController {

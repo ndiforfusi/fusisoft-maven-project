ee72b1bc6b91cb5759452226fc9e5a61146948d1
Ndiforamang Fusi <fusisoft@gmail.com>
2024-10-14 13:38:16 -0500

    added images for consulting websites

--- src/main/java/com/mt/controllers/HomeController.java
+++ src/main/java/com/mt/controllers/HomeController.java
@@ -1,15 +1,17 @@
 package com.mt.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
 
-    @RequestMapping("/home")
-    public ModelAndView showHomePage() {
-        return new ModelAndView("home");  // Refers to WEB-INF/jsps/home.jsp
+    @GetMapping("/home")
+    public ModelAndView homePage() {
+        return new ModelAndView("home"); // Maps to /WEB-INF/jsps/home.jsp
     }
 }
+
+
 
